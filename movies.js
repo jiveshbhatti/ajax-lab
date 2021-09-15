9d0dd2e3d59140f64f9873a11f52f161d17f16f4
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2021-09-15 11:38:05 -0700

    updated year spacing

--- movies.js
+++ movies.js
@@ -1,52 +1,34 @@
-console.log('hi')
-
-const baseUrl = `http://www.omdbapi.com/?apikey=24649845&s=`
 
 
-async function movie(){
+const baseUrl = `http://www.omdbapi.com/?apikey=24649845&s=`;
 
-   const searchTitle = document.getElementById('name').value
- 
-  console.log(searchTitle)
-   
- const movies = await fetch(`${baseUrl}${searchTitle}`).then(response => response.json())
+async function movie() {
+  const searchTitle = document.getElementById("name").value;
 
+  console.log(searchTitle);
 
-console.log(movies.Search[0].Title,'within first function')
+  const movies = await fetch(`${baseUrl}${searchTitle}`).then((response) =>
+    response.json()
+  );
 
-render(movies)
-return movies
+  console.log(movies.Search[0].Title, "within first function");
 
-
+  render(movies);
+  return movies;
 }
 
+function render(movies) {
+  console.log(movies.Search, "one movie");
+  const results = movies.Search;
 
+  results.forEach((element) => {
+    console.log(element.Title);
+    const titles = element.Title;
+    const year = element.Year;
 
-function render(movies){
-
- //console.log(movies.Search, 'this is the information being passed in')
-console.log(movies.Search, 'one movie')
- const results = movies.Search
-//  let allTitles = []
- results.forEach(element => {
-    // document.getElementById('results') .appendChild()
-    // let list = document.createElement('li').appendChild()
-    console.log(element.Title)
-    const titles = element.Title
-    const year = element.Year
-  
-    // allTitles.push(titles)
-
-   
-    //  document.getElementById('results').textContent = `<ol><li>${titles}</li></ol>`
-    // document.getElementById('results').insertAdjacentHTML('beforeend',`<ol><li>${element.Year}</li></ol>`)
-     //$('#results').html(allTitles)
-    $('#results').append(`<li>${titles}${year}</li>`, `<img src=${element.Poster}>`)
-
-    //  console.log(year)
-    // console.log(allTitles)
- });
-
-//  $('#results').append(`<ol><li>${results}</li></ol>`);
+    $("#results").append(
+      `<h3>${titles}, ${year}</h3>`,
+      `<img src=${element.Poster}>`
+    );
+  });
 }
-
